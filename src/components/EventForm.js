bcdb3844b6841dc80305a818158f6fe0da19402b
nobmurakita <nobmurakita@users.noreply.github.com>
2022-01-13 16:56:35 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -2,9 +2,12 @@ import React, { useContext, useState } from 'react';
 
 import {
   CREATE_EVENT,
-  DELETE_ALL_EVENTS
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS
 } from '../actions';
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -20,6 +23,12 @@ const EventForm = () => {
       body
     });
 
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601()
+    });
+
     setTitle('');
     setBody('');
   };
@@ -29,6 +38,12 @@ const EventForm = () => {
     const result = window.confirm('全てのイベントを本当に削除しても良いですか？');
     if (result) {
       dispatch({ type: DELETE_ALL_EVENTS });
+
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを作成しました。',
+        operatedAt: timeCurrentIso8601()
+      })
     }
   };
 
